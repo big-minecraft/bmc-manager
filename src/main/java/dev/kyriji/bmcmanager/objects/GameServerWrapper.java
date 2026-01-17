b1c544fb5abfe5635e28e5b41ff093612fc200c6
wiji <wijigaming.yt@gmail.com>
2026-01-17 01:23:04 -0800

    add logging

--- src/main/java/dev/kyriji/bmcmanager/objects/GameServerWrapper.java
+++ src/main/java/dev/kyriji/bmcmanager/objects/GameServerWrapper.java
@@ -52,10 +52,16 @@ public abstract class GameServerWrapper<T extends Instance> implements Scalable 
 	}
 
 	public void fetchInstances() {
+		int previousCount = this.instances.size();
 		this.instances.clear();
 
 		List<Instance> instances = RedisManager.get().scanAndDeserializeInstances("instance:*:" + name);
 		this.instances.addAll((Collection<? extends T>) instances);
+
+		// Log if instance count dropped unexpectedly (helps diagnose disappearing instances)
+		if (previousCount > 0 && this.instances.isEmpty()) {
+			System.err.println("WARNING: " + name + " went from " + previousCount + " instances to 0 after fetchInstances()!");
+		}
 	}
 
 	public String getName() {
